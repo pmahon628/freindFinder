4d0c2f16b38f0ff9278e4ed3ef024cd4ddb00f55
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-06-20 22:56:55 -0400

    added arrays

---
+++ freinds.js
@@ -0,0 +1,21 @@
+//holds array of objects
+//name, photo, scores
+
+ var Freind = [
+   { name = "Jake",
+     scores = [3,2,4,2,1,3,5,4,3,2],
+ },
+
+ {
+     name = "Brendan",
+     scores = [2,4,1,2,4,5,3,2,1,3],
+ },
+
+ {
+     name = "Joel",
+     scores = [1,3,5,5,5,3,5,4,2,2],
+ }
+];
+
+
+

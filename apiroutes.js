b3e8ee06030494569a330ef69a74da2b82b3ead1
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-06-22 16:41:26 -0400

    set up inital routing, still more to do

--- apiroutes.js
+++ apiroutes.js
@@ -2,3 +2,17 @@
 // A POST routes /api/friends. 
 // This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
+
+var surveryData = require("../data/surveryData");
+
+module.exports = function(app){
+
+app.get('/api/freinds', function (reqm res){
+        res.json(freindsData);
+    });
+
+app.post('/api/freinds', function(req, res){
+    res.json(surveryData);
+});
+
+}

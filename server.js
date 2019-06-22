d73aa849da0a04f399e2d5abe42e7491a13339ca
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-06-21 22:46:13 -0400

    worked on routing

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-//- server.js  - express and path npm packages
+var express = require("express");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/freinds" function(req, res){
+    var chosen = req.params.character;
+    console.log(chosen);
+
+    return res.json(characters[i]);
+
+
+});

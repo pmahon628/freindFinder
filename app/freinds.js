5d297d21eebe7f89cad2c994a1963ccd0c124337
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-07-21 21:53:39 -0400

    work

--- app/freinds.js
+++ app/freinds.js
@@ -1,28 +1,30 @@
 //holds array of objects 
 //name, photo, scores
 
- var freinds = [
-   { name = "Jake",
-     scores = [3,2,4,2,1,3,5,4,3,2],
+ var freindsArray = [
+   
+    { 
+     name: "Jake",
+     scores: ['3','2','4','2','1','3','5','4','3','2'],
  },
 
  {
-     name = "Brendan",
-     scores = [2,4,1,2,4,5,3,2,1,3],
+     name: "Brendan",
+     scores: ['2','4','1','2','4','5','3','2','1','3'],
  },
 
  {
-     name = "Joel",
-     scores = [1,3,5,5,5,3,5,4,2,2],
+     name: "Joel",
+     scores: ['1','3','5','5','5','3','5','4','2','2'],
  },
  
  {
-    name = "Ryan",
-    scores = [2,2,5,4,2,1,3,2,3,4],
+    name: "Ryan",
+    scores: ['2','2','5','4','2','1','3','2','3','4'],
  },
 ];
 
-module.exports = freinds;
+module.exports = freindsArray;
 
 
 
